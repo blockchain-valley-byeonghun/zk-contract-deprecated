e6f75c0e447ee982d4c39f8e0ddb6ff0de5324b4
ByeongHunKim <rkwhr912@naver.com>
2023-07-31 19:26:44 +0900

    script : deploy feedback contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,22 +3,21 @@ import { getDatetimeString } from '../utils/timeUtils';
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now());
   const contractCreatedTime = getDatetimeString(currentTimestampInSeconds)
-  const unlockTime = currentTimestampInSeconds + 60;
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime);
+  const Feedback = await ethers.getContractFactory("Feedback");
+  const feedback = await Feedback.deploy('0x40A6ad127e3b4C8077af42a2437cCbd3cdC7609f', '0x6d7DEFc10BA387497fc5e8B2C03Ae13ef8bd5403');
 
-  await lock.deployed();
+  await feedback.deployed();
 
   console.log(
-    `${lock.address} contract is deployed at ${contractCreatedTime}`
+    `${feedback.address} contract is deployed at ${contractCreatedTime}`
   );
   console.log(`
  click this link if you deploy contract on Ethereum Sepolia Testnet
- https://sepolia.etherscan.io/address/${lock.address} \n
+ https://sepolia.etherscan.io/address/${feedback.address} \n
  click this link if you deploy contract on Bianace Smart Chain Testnet
- https://testnet.bscscan.com/address/${lock.address} \n
+ https://testnet.bscscan.com/address/${feedback.address} \n
  click this link if you deploy contract on Polygon Mumbai Testnet
- https://mumbai.polygonscan.com/address/${lock.address} \n
+ https://mumbai.polygonscan.com/address/${feedback.address} \n
   `)
 }
 
